e82d3a6252518ea8684f7c27772b3eedf56b2fb0
tao.pei <gpeitao@gmail.com>
2017-12-14 15:46:38 +0800

    envsetup

--- envsetup.py
+++ envsetup.py
@@ -7,16 +7,20 @@ import platform
 os.system('python -m pip install selenium')
 
 
-def mk_tool_dir():
+def get_tool_dir():
     sys_name = platform.system()
     user_home = os.environ['userprofile'] if sys_name == 'Windows' else os.environ['HOME']
     dr = os.path.join(user_home, '.gitdull')
-    if os.path.exists(dr):
-        return
-    os.mkdir(dr)
+    if not os.path.exists(dr):
+        os.mkdir(dr)
+    return dr
 
 
-
-
-
+if __name__ == "__main__":
+    path = get_tool_dir()
+    if not os.listdir(path):
+        cmd = 'git clone https://github.com/jypeitao/WebInteraction.git -b dulltool ' + path
+        os.system(cmd)
+    else:
+        print(path, " exist.")
 
